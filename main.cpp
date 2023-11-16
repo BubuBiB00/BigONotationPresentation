0b1d8551a589aaff3ff8ab976b16752729424883
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2023-11-17 00:48:06 +0100

    done

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int bob_solution(int n){
             for (int c = 0; c <= n; c++){
                 if (a + b + c == n){
                     solutions ++;
-                    //cout << "["<< a <<","<< b << "," << c << "]" << endl;
+                    cout << "["<< a <<","<< b << "," << c << "]" << endl;
                     // grosser einfluss auf die Laufzeit!
                 }
                 else {
@@ -30,7 +30,7 @@ int alice_solution(int n){
             int c = n - (a+b);
             if (c >= 0){
                 solutions ++;
-                //cout << "["<< a <<","<< b << "," << c << "]" << endl;
+                cout << "["<< a <<","<< b << "," << c << "]" << endl;
                 // grosser einfluss auf die Laufzeit!
             }
         }
@@ -38,21 +38,30 @@ int alice_solution(int n){
     return solutions;
 }
 
+
+int f(int n){
+    if (n==1){
+        return 1;
+    }
+    else{
+        return f(n-1) + f(n-1);
+    }
+}
+
 int main(){
-    int n = 10;
+    int n = 5;
 
     auto start_alice = high_resolution_clock::now();
     auto alices_solution = alice_solution(n);
     auto stop_alice = high_resolution_clock::now();
-    auto duration_alice = duration_cast<seconds>(stop_alice - start_alice);
-    cout << "Alices Solution "<< alices_solution <<" took " << duration_alice.count() << " seconds to calculate!" << endl;
+    auto duration_alice = duration_cast<milliseconds>(stop_alice - start_alice);
+    cout << "Alices Solution: " << alices_solution <<" number combinations, took " << duration_alice.count() << " milliseconds to calculate!" << endl;
 
     auto start_bob = high_resolution_clock::now();
     auto bobs_solution = bob_solution(n);
     auto stop_bob = high_resolution_clock::now();
-    auto duration_bob = duration_cast<seconds>(stop_bob - start_bob);
-    cout << "Bobs Solution " << bobs_solution << " took " << duration_bob.count() << " seconds to caluculate!" << endl;
-    
+    auto duration_bob = duration_cast<milliseconds>(stop_bob - start_bob);
+    cout << "Bobs Solution: " << bobs_solution << " number combinations, took " << duration_bob.count() << " milliseconds to caluculate!" << endl;
 
     return 0;
 }
