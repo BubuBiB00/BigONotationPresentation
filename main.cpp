286741c57aa9e0093ca1acf9b0eefbe06f3d253c
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2023-11-16 20:29:04 +0100

    done O Notation

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream> 
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+int bob_solution(int n){
+    int solutions = 0;
+    for (int a = 0; a <= n; a++){
+        for (int b = 0; b <= n; b++){
+            for (int c = 0; c <= n; c++){
+                if (a + b + c == n){
+                    solutions ++;
+                    //cout << "["<< a <<","<< b << "," << c << "]" << endl;
+                }
+                else {
+                    continue;     
+                }
+            }
+        }   
+    }
+    return solutions;
+}
+
+int alice_solution(int n){
+    int solutions = 0;
+    for (int a = 0; a <= n; a++){
+        for (int b = 0; b <= n; b++){
+            int c = n - (a+b);
+            if (c >= 0){
+                solutions ++;
+                //cout << "["<< a <<","<< b << "," << c << "]" << endl;
+            }
+        }
+    }
+    return solutions;
+}
+
+int main(){
+    int test = 2500;
+
+    auto start_alice = high_resolution_clock::now();
+    auto alices_solution = alice_solution(test);
+    auto stop_alice = high_resolution_clock::now();
+    auto duration_alice = duration_cast<seconds>(stop_alice - start_alice);
+    cout << "Alices Solution "<< alices_solution <<" took " << duration_alice.count() << " seconds to calculate!" << endl;
+
+    auto start_bob = high_resolution_clock::now();
+    auto bobs_solution = bob_solution(test);
+    auto stop_bob = high_resolution_clock::now();
+    auto duration_bob = duration_cast<seconds>(stop_bob - start_bob);
+    cout << "Bobs Solution " << bobs_solution << " took " << duration_bob.count() << " seconds to caluculate!" << endl;
+
+    return 0;
+}
+
